333886a4348d109914c15a9e785891d4bb32384a
Oscar <oscarantonio.soto1998@gmail.com>
2022-11-11 14:35:00 -0300

    Fix: add express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hola mundo')
+const express = require('express');
+
+// Crear el servidor de express
+const app = express();
+
+// Rutas
+app.get('/', (req, res) => {
+  res.json({
+    ok: true,
+  });
+});
+
+// Ecuchar peticiones
+app.listen(4000, () => {
+  console.log(`servior corriendo en puerto ${4000}`);
+});
