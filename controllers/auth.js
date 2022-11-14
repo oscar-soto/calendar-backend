dc360c3313fdaae0c4e0f780ed94de30ba6c2e70
Oscar <oscarantonio.soto1998@gmail.com>
2022-11-14 18:00:11 -0300

    Fix: User validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,15 +2,27 @@ const { response } = require('express');
 const User = require('../models/User');
 
 const newUser = async (req, res = response) => {
-  // const { name, email, password } = req.body;
+  const { email, password } = req.body;
 
-  const user = new User(req.body);
   try {
+    let user = await User.findOne({ email });
+
+    // Email exists
+    if (user) {
+      res.status(400).json({
+        ok: false,
+        msg: 'El usuario ya existe con ese correo',
+      });
+    }
+
+    // Save a new user
+    user = new User(req.body);
     await user.save();
 
     res.status(201).json({
       ok: true,
-      msg: 'Register',
+      uid: user.id,
+      name: user.name
     });
   } catch (error) {
     console.log(error);
