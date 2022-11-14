c6e10556c03fb010cf179eaa32cc06f7187c9dfc
Oscar <oscarantonio.soto1998@gmail.com>
2022-11-14 13:52:57 -0300

    Fix: express validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,19 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 const newUser = (req, res = response) => {
   const { name, email, password } = req.body;
 
-  if (name.length < 5) {
+  // Manejo de errores
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
     return res.status(400).json({
       ok: false,
-      msg: 'El nombre debe ser de 5 letras',
+      errors: errors.mapped(),
     });
   }
 
-  res.json({
+  res.status(201).json({
     ok: true,
     msg: 'Register',
     name,
@@ -21,6 +24,15 @@ const newUser = (req, res = response) => {
 
 const loginUser = (req, res = response) => {
   const { email, password } = req.body;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped(),
+    });
+  }
 
   res.json({
     ok: true,
--- routes/auth.js
+++ routes/auth.js
@@ -4,12 +4,28 @@
 */
 
 const { Router } = require('express');
+const {check} = require('express-validator')
 const router = Router();// const router = express.Router
 
 const { newUser, loginUser, renewToken } = require('../controllers/auth');
 
-router.post('/new', newUser);
-router.post('/', loginUser);
+router.post(
+  '/new',
+  [ // Middlewares
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe de ser de 6 caracteres').isLength({min: 6}),
+  ], 
+  newUser
+);
+router.post(
+  '/', 
+  [
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password incorrecto').isLength({min: 6}),
+  ],
+  loginUser
+);
 router.get('/renew', renewToken);
 
 module.exports = router;
