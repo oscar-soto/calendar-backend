d7fc4dd18f587e358438617ea4c7694c4737555e
Oscar <oscarantonio.soto1998@gmail.com>
2022-11-14 15:50:29 -0300

    Fix: new user to our DB

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,25 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
+const User = require('../models/User');
 
-const newUser = (req, res = response) => {
-  const { name, email, password } = req.body;
+const newUser = async (req, res = response) => {
+  // const { name, email, password } = req.body;
 
-  res.status(201).json({
-    ok: true,
-    msg: 'Register',
-    name,
-    email,
-    password,
-  });
+  const user = new User(req.body);
+  try {
+    await user.save();
+
+    res.status(201).json({
+      ok: true,
+      msg: 'Register',
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      ok: false,
+      msg: 'Porfavor hable con el administrador',
+    });
+  }
 };
 
 const loginUser = (req, res = response) => {
